e5cd2bc9d8db91f1cf9af64b50a183ded876638c
ROlearyPro <rolearyprogramming@gmail.com>
2024-09-10 18:07:33 -0600

    Cypress weather stuff, to be merged
    Co-authored-by: Zelalem Fitru <ZTFitru@users.noreply.github.com>
    Co-authored-by: Seth <seth.r.way@gmail.com>

--- cypress/e2e/weather_display.spec.cy.js
+++ cypress/e2e/weather_display.spec.cy.js
@@ -1,22 +1,35 @@
+import {
+  boomHappyQuotes,
+} from '../../src/assets/boomhauer_quotes/boomhauerQuotes.json';
 describe('template spec', () => {
   beforeEach(() => {
-  cy.intercept(
-    'https://maps.googleapis.com/maps/api/mapsjs/gen_204?csp_test=true'
-  ).as('Google API Connection');
-  cy.intercept('https://api.openweathermap.org/data/2.5/forecast?lat=40.4417&lon=-79.9901&units=imperial&appid=8d90eba3b2dd0278b950d5c6fbe2725d').as('Get 5 Day');
-  cy.visit('http://localhost:5173/location/40.4416941/-79.9900861');
-});
-it('should correctly display the weather for the given location',()=>
-{
-  cy.get('.text-lg').contains('Pittsburgh')
-  cy.get('.art')
+    cy.intercept('GET',
+      'https://maps.googleapis.com/maps/api/mapsjs/gen_204?csp_test=true'
+    ).as('location');
+    cy.intercept('GET', 'https://api.openweathermap.org/data/2.5/forecast?lat=40.4417&lon=-79.9901&units=imperial&appid=882333ffa9f50691f0a4fc01f80d4c92', { statusCode: 200, fixture: 'Weather' }).as('Get 5 day');
+    
+    cy.visit('http://localhost:5173/location/40.4416941/-79.9900861');
 
-})
-it('should correctly choose a quote/image appropriate for the weather', ()=>
-{
+  });
 
-})
+  it('should correctly identify an invalid date and react accordingly', () => {
+    cy.intercept('GET', 'https://api.openweathermap.org/data/2.5/forecast?lat=40.4417&lon=-79.9901&units=imperial&appid=8d90eba3b2dd0278b950d5c6fbe2725d', { statusCode: 200, fixture: 'Weather' }).as('Get 5 day');
+    cy.visit('http://localhost:5173/location/40.4416941/-79.9900861');
+    
+    cy.get(':nth-child(2) > .italic').contains("Invalid Date")
+    cy.get('.quote').contains(`""Tell y'what man, you talk'n bout ol' meanin' ah life, man, go read tha' ol hitchiker's guide, man, y'know talkin' bout ol, 42, man."" -Boomhauer`)
+    
+  })
 
+  it('should correctly choose a quote/image appropriate for the weather', () => {
+    
+    cy.get('.text-center > :nth-child(2) > :nth-child(1)').contains("conditions: Clear");
+    cy.get('.quote')
+  })
+
+  it('should correctly choose a quote/image appropriate for the weather', () => {
+    
+  })
 
 });
 
